979b02a362cc2566f12ece4ed7f886ab2c4cd5c0
Jthomas3425 <jthomas0756@gmail.com>
2017-06-04 19:23:17 -0700

    Updated Landing EJS

--- index.js
+++ index.js
@@ -2,13 +2,15 @@ var express = require("express");
 var path = require("path")
 var Server = express();
 
-Server.use(express.static("views/Resources"))
+Server.use(express.static("views/Resources/img"))
+Server.use(express.static("views/Resources/css")
+Server.use(express.static("views/Resources/js")
 
 Server.get("/",function(req,res){
   console.log("Home page was requested.")
   res.render("Landing.ejs")
 });
 
-Server.listen(3000,function(){	
+Server.listen(3000,function(){
   console.log("Server is listening on Port 3000");
 });
