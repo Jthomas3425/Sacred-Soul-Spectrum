4b1b89fdcb8a3114444fac0e96b26442109de6f6
Jthomas3425 <jthomas0756@gmail.com>
2017-06-04 18:32:43 -0700

    Updated Server-Side code

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 var express = require("express");
+var path = require("path")
 var Server = express();
 
-Server.use(express.static('views/Resources'))
+Server.use(express.static("views/Resources"))
 
 Server.get("/",function(req,res){
   console.log("Home page was requested.")
   res.render("Landing.ejs")
 });
 
-Server.listen(3000,function(){
+Server.listen(3000,function(){	
   console.log("Server is listening on Port 3000");
 });
