d0ead9f908a52fd3cdd77857c9e78eb52743d997
Jthomas3425 <jthomas0756@gmail.com>
2017-06-04 19:24:38 -0700

    Typo/Bugs....

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ var express = require("express");
 var path = require("path")
 var Server = express();
 
-Server.use(express.static("views/Resources/img"))
-Server.use(express.static("views/Resources/css")
-Server.use(express.static("views/Resources/js")
+Server.use(express.static("views/Resources/img"));
+Server.use(express.static("views/Resources/css"));
+Server.use(express.static("views/Resources/js"));
 
 Server.get("/",function(req,res){
   console.log("Home page was requested.")
